9fdcc535aced881cc4640c79f6c3edc50ec42d63
EltonVale <elton.vale97@gmail.com>
2023-06-09 17:39:44 -0400

    Subindo o projeto

---
+++ cadastro/app_cad_usuarios/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Usuario 
+
+def home(request):
+    return render(request,'usuarios/home.html')
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+# Exibir usuarios ja cadastrados
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+# Retornar os dados para a página de listagem
+    return render(request, 'usuarios/usuarios.html', usuarios)
